88a9403198003b09bc2f3188d6baf914362d5cda
md shoharab pk <shoharabpk.2019@gmail.com>
2021-05-28 04:51:42 +0600

    sidebar complete by shoharab

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -7,11 +7,20 @@ import './style/_app.scss'
 
 
 const App = () => {
+    
+    const [sidebar, toggleSidebar] = useState(false)
+console.log(sidebar)
+    const handleToggleSidebar = () =>  {toggleSidebar(value => !value)
+    console.log("clicked")
+    }
+
+
+
     return (
         <>
-            <Header />
+            <Header handleToggleSidebar={handleToggleSidebar} />
             <div className="app__container border border-info">
-                <Sidebar />
+                <Sidebar sidebar={sidebar} />
 
                 <Container fluid className="app__main border border-warning">
                     <HomeScreen />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,10 @@ import {FaBars} from 'react-icons/fa';
 import {AiOutlineSearch} from 'react-icons/ai';
 import {MdNotifications, MdApps} from 'react-icons/md';
 
-const Header = () => {
+const Header = ({handleToggleSidebar}) => {
     return (
         <div className="border border-dark header">
-             <FaBars className="header__menu" size={26} />
+             <FaBars onClick={handleToggleSidebar} className="header__menu" size={26} />
              <img src="http://pngimg.com/uploads/youtube/youtube_PNG2.png" alt="" className="header__logo" />
         <form action="">
             <input type="text" placeholder="Search" />
