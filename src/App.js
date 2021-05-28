cb10a09ea05114fb1ff258e2ed714deff7a3f317
md shoharab pk <shoharabpk.2019@gmail.com>
2021-05-28 16:37:06 +0600

    routing complete by shoharab

--- src/App.js
+++ src/App.js
@@ -3,32 +3,60 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import HomeScreen from './screen/HomeScreen/HomeScreen';
-// import LoginScreen from './screen/LoginScreen/LoginScreen';
-import './style/_app.scss'
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+import './style/_app.scss';
+import LoginScreen from './screen/LoginScreen/LoginScreen';
 
+
+const Leyout = ({children}) =>  {
+    const [sidebar, toggleSidebar] = useState(false)
+    console.log(sidebar)
+        const handleToggleSidebar = () =>  {toggleSidebar(value => !value) 
+        }
+    return( 
+        <>
+        <Header handleToggleSidebar={handleToggleSidebar} />
+        <div className="app__container border border-info">
+            <Sidebar   sidebar={sidebar} handleToggleSidebar={handleToggleSidebar}  />
+
+            <Container fluid className="app__main border border-warning">
+               {children}
+            </Container>
+        </div>
+
+    </>
+    )
+}
 
 const App = () => {
     
-    const [sidebar, toggleSidebar] = useState(false)
-console.log(sidebar)
-    const handleToggleSidebar = () =>  {toggleSidebar(value => !value)
-    console.log("clicked")
-    }
+ 
 
 
 
     return (
-        <>
-            <Header handleToggleSidebar={handleToggleSidebar} />
-            <div className="app__container border border-info">
-                <Sidebar   sidebar={sidebar} handleToggleSidebar={handleToggleSidebar}  />
+      <Router>
+          <Switch>
+                <Route exact path="/">
+                    <Leyout>
+                        <HomeScreen />
+                    </Leyout>
+                </Route>
 
-                <Container fluid className="app__main border border-warning">
-                    <HomeScreen />
-                </Container>
-            </div>
- 
-        </>
+                <Route path="/auth">
+                    <LoginScreen></LoginScreen>
+                </Route>
+                <Route path="/search">
+                     <h1>Search Result</h1>
+                </Route>
+
+
+
+                <Route>
+                    <Redirect to="/" />
+                 </Route> 
+          </Switch>
+      </Router>
     );
 };
 
---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDeM8jjQln_ZGyaSay3h6IjGAWf7QAYolE",
+    authDomain: "yt-video-team-25.firebaseapp.com",
+    projectId: "yt-video-team-25",
+    storageBucket: "yt-video-team-25.appspot.com",
+    messagingSenderId: "1091152982606",
+    appId: "1:1091152982606:web:00b115cd7d5df92934891f"
+  };
+
+  firebase.initializeApp(firebaseConfig)
+  export default firebaseConfig;
