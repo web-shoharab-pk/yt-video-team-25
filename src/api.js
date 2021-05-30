45567856ee93d2e7772526f0ca93d725f8762a1a
md shoharab pk <shoharabpk.2019@gmail.com>
2021-05-30 21:52:44 +0600

    env setup

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
-
+require('dotenv').config()
+ 
 
 const request = axios.create({
     baseURL: "https://youtube.googleapis.com/youtube/v3/",
     params: {
-        key: "AIzaSyDeM8jjQln_ZGyaSay3h6IjGAWf7QAYolE",
+        key: process.env.YT_API_KEY,
     },
+    
 }) 
-export default request;
+export default request; 
